c0c0b607439bba7ba81493c9227f5a89f9c9eb24
hebeormsby <hebeormsby@gmail.com>
2024-10-12 20:55:22 +1300

    update

--- scripts/menu.js
+++ scripts/menu.js
@@ -4,5 +4,5 @@ const navigation = document.querySelector('.navigation');
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
-	navigation.setAttribute('aria-hidden', nav.classList.contains('open') ? 'false' : 'true');
+	navigation.setAttribute('aria-hidden', navigation.classList.contains('open') ? 'false' : 'true');
 });
